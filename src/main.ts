a3f1478b56552733571b850474cd4ee03a425cb8
Tristan de Jong <trisdejong777@gmail.com>
2024-12-11 00:55:27 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,24 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'myNotes';
+}
+
+bootstrapApplication(App);
+@Component({
+  selector: 'app-root',
+  standalone: true,
   templateUrl: `./app.component.html`,
   styleUrl: `./app.component.css`,
 })
-export class AppComponent { }
+export class AppComponent {}
 
 bootstrapApplication(AppComponent);
